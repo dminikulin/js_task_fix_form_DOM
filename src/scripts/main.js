ae0abaa4bf2d4fd520519d86c3920ed2c67122a2
dminikulin <dminikulin2003@gmail.com>
2025-06-01 15:13:17 +0300

    Fixed the name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,14 @@ const inputs = [...document.querySelectorAll('input')];
 
 inputs.forEach((i) => {
   const newLabel = document.createElement('label');
+  const capitalizedName = i.name.charAt(0).toUpperCase() + i.name.substring(1);
 
   newLabel.classList.add('field-label');
-  newLabel.textContent = i.name;
+  newLabel.textContent = capitalizedName;
 
   if (i.id) {
     newLabel.setAttribute('for', i.id);
   }
-
-  const capitalizedName = i.name.charAt(0).toUpperCase() + i.name.substring(1);
 
   i.setAttribute('placeholder', capitalizedName);
 
